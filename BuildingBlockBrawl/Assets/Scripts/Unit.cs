f2c9ae0346cb2101aa1fdf92e3d1a8cff572245f
Winson Weng (RIT Student) <wqw7732@rit.edu>
2023-10-16 14:03:51 -0400

    Initial unit setup

---
+++ BuildingBlockBrawl/Assets/Scripts/Unit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string name;
+    [SerializeField] private float health;
+    [SerializeField] private float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
